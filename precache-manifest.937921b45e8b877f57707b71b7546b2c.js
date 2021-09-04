e6b2ba65683874e5ee5d29a68823eb531602a2db
Noel De Martin <noeldemartin@gmail.com>
2021-09-04 13:50:46 +0200

    deploy

---
+++ precache-manifest.937921b45e8b877f57707b71b7546b2c.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4917a69e37d26bb53632bc88cc4f5fb",
+    "url": "/media-kraken/404.html"
+  },
+  {
+    "revision": "e2d70ace7fad2d4becae",
+    "url": "/media-kraken/css/chunk-common.4df6666d.css"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/media-kraken/fonts/Montserrat_Bold_Latin.39d93cf6.woff2"
+  },
+  {
+    "revision": "86a4da42c3dda46f89b6884198c1ee0d",
+    "url": "/media-kraken/fonts/Montserrat_Bold_Latin_Ext.86a4da42.woff2"
+  },
+  {
+    "revision": "92d16e458625f4d2c8940f6bdca0ff09",
+    "url": "/media-kraken/fonts/Montserrat_Medium_Latin.92d16e45.woff2"
+  },
+  {
+    "revision": "28f17473fa4ea5319c182e3ba687a983",
+    "url": "/media-kraken/fonts/Montserrat_Medium_Latin_Ext.28f17473.woff2"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/media-kraken/fonts/Montserrat_Regular_Latin.bc3aa95d.woff2"
+  },
+  {
+    "revision": "97638b9a50d86ae960e84043314bf07a",
+    "url": "/media-kraken/fonts/Montserrat_Regular_Latin_Ext.97638b9a.woff2"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/media-kraken/fonts/Montserrat_SemiBold_Latin.6fb1b562.woff2"
+  },
+  {
+    "revision": "f44682aac1828f936e6d11685aa391c8",
+    "url": "/media-kraken/fonts/Montserrat_SemiBold_Latin_Ext.f44682aa.woff2"
+  },
+  {
+    "revision": "b1611a6c5b6ef99668a81fc8ebf42058",
+    "url": "/media-kraken/js/0.38c8be50.worker.js"
+  },
+  {
+    "revision": "e2ad404620386229393d",
+    "url": "/media-kraken/js/404.3d9e2093.js"
+  },
+  {
+    "revision": "8228de2b029d518dda89",
+    "url": "/media-kraken/js/authentication-inrupt.4f5fc39f.js"
+  },
+  {
+    "revision": "2152dec4fcdb15d3cf505323d002d55b",
+    "url": "/media-kraken/js/authentication-inrupt.695f942d.worker.js"
+  },
+  {
+    "revision": "6adfe1ea112b5b0591cd",
+    "url": "/media-kraken/js/authentication-legacy.1a6a5e7c.js"
+  },
+  {
+    "revision": "891a9d9663c5c5b997ec0b03944a290f",
+    "url": "/media-kraken/js/authentication-legacy.2a02f0bd.worker.js"
+  },
+  {
+    "revision": "125cfa4d3666ad043fb3",
+    "url": "/media-kraken/js/chunk-2d0a3756.faa42f8e.js"
+  },
+  {
+    "revision": "02766d8a82cf1442801c",
+    "url": "/media-kraken/js/chunk-2d0aa213.d1115757.js"
+  },
+  {
+    "revision": "2793fd9fd2711e307aa4",
+    "url": "/media-kraken/js/chunk-2d0c77ae.97543542.js"
+  },
+  {
+    "revision": "02253c51b7508782e85f",
+    "url": "/media-kraken/js/chunk-2d0d7e72.d7cf0931.js"
+  },
+  {
+    "revision": "649708a0a1534f628343",
+    "url": "/media-kraken/js/chunk-2d0e57f0.00126bb1.js"
+  },
+  {
+    "revision": "c84304505dd7526dbf28",
+    "url": "/media-kraken/js/chunk-2d0e68d7.6e16f6f8.js"
+  },
+  {
+    "revision": "d462c9ecae4fe4504476",
+    "url": "/media-kraken/js/chunk-2d229d4e.2a80c4e5.js"
+  },
+  {
+    "revision": "e2d70ace7fad2d4becae",
+    "url": "/media-kraken/js/chunk-common.16a59b7c.js"
+  },
+  {
+    "revision": "668ee94c7b7d7ba6b0be",
+    "url": "/media-kraken/js/chunk-vendors.9901eb0e.js"
+  },
+  {
+    "revision": "6d4f6b60d26dae8c1283",
+    "url": "/media-kraken/js/index.2de975e2.js"
+  },
+  {
+    "revision": "79591b90ecdffd7fd62c",
+    "url": "/media-kraken/js/viewer.16c586b6.js"
+  },
+  {
+    "revision": "85069fda3901494f8230f5a4a1de12cf",
+    "url": "/media-kraken/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/media-kraken/robots.txt"
+  },
+  {
+    "revision": "d7a37d99a2ff213c879a364f76719ecf",
+    "url": "/media-kraken/viewer.html"
+  }
+]);
